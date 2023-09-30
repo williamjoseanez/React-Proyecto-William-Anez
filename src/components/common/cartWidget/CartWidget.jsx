3529a6f793ccc1b13adf77249050dd07e3634bcf
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-09-30 18:49:35 -0300

    agregando props

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -6,7 +6,7 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 const CartWidget = () => {
   return (
     <div>
-      <Badge badgeContent={0} showZero color="primary">
+      <Badge badgeContent={5} showZero color="primary">
         <ShoppingCartIcon  color="action" />
       </Badge>
       
