df46b500f3af36e09715707967f40c42dfa1c07a
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-10-02 12:00:02 -0300

    borrando greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import Greeting from "./components/common/Greeting/Greeting";
 import CounterContainer from "./components/common/counter/CounterContainer";
 import { Navbar } from "./components/layout/navbar/Navbar";
 import MaterialUi from "./components/materialUi/MaterialUi";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
-  
   return (
     <>
       <Navbar />
       <ItemListContainer saludo={` "Bienvenido"`} />
-      <Greeting props={ "Greeting, sacado de internet"} />
       <CounterContainer />
       <MaterialUi />
-    
     </>
   );
 }
-
 
 export default App;
--- src/components/common/Greeting/Greeting.jsx
+++
@@ -1,8 +0,0 @@
-
-const UserGreeting = ({ props }) => {
-    return <div>
-       <h3 className="center">Greeting {props}</h3> 
-    </div>;
-};
-
-export default UserGreeting;
